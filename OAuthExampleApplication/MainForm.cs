ec18e4665b67ddc4cafbb7f597a788aa416460d1
Deshmukh, Ketan <Ketan.Deshmukh@wolterskluwer.com>
2023-03-27 15:42:59 +0530

    [MTN-27836] Use file scoped namespaces

--- OAuthExampleApplication/MainForm.cs
+++ OAuthExampleApplication/MainForm.cs
@@ -4,61 +4,60 @@ using System.Linq;
 using System.Windows.Forms;
 using System.Xml.Linq;
 
-namespace OAuth2ExampleApp
+namespace OAuth2ExampleApp;
+
+public partial class MainForm : Form
 {
-	public partial class MainForm : Form
+	public MainForm() => InitializeComponent();
+
+	void btnAuthorize_Click(object sender, EventArgs e)
 	{
-		public MainForm() => InitializeComponent();
+		var clientId = ConfigurationManager.AppSettings["ClientId"];
+		var redirectUri = ConfigurationManager.AppSettings["RedirectUri"];
 
-		void btnAuthorize_Click(object sender, EventArgs e)
+		var result = AuthorizeDialog.Show(clientId, redirectUri);
+		if (result != null && result.Success)
 		{
-			var clientId = ConfigurationManager.AppSettings["ClientId"];
-			var redirectUri = ConfigurationManager.AppSettings["RedirectUri"];
 
-			var result = AuthorizeDialog.Show(clientId, redirectUri);
-			if (result != null && result.Success)
-			{
-
-				Log($"Authorized user {result.User} in organization {result.Organisation}.");
-				DisplayCompanies(result.AccessToken, result.ClusterUrl);
-			}
-			else
-				Log($"Authorization failed: {result?.Message}");
+			Log($"Authorized user {result.User} in organization {result.Organisation}.");
+			DisplayCompanies(result.AccessToken, result.ClusterUrl);
+		}
+		else
+			Log($"Authorization failed: {result?.Message}");
 			
-		}
-
-		void DisplayCompanies(string accessToken, string clusterUrl)
-		{
-			var processXmlService = new ProcessXmlService(clusterUrl);
-
-			var result = processXmlService.GetCompanies(accessToken);
-
-			var doc = XDocument.Parse(result);
-			var companies = doc.Root?.Elements("office").ToArray();
-
-			Log($"Access token : {accessToken}");
-			AppendNewLine();
-			Log($"Cluster url : {clusterUrl}");
-			AppendNewLine();
-			Log($"Found {companies?.Length} companies");
-			AppendNewLine();
-			Log($"Displaying first 10 companies");
-			AppendNewLine();
-
-			if (companies == null || !companies.Any()) 
-				return;
-
-			foreach (var company in companies.Take(10))
-				Log($"{company.Attribute("name")?.Value} [{company.Value}]");
-		}
-
-		void Log(string text)
-		{
-			txtLog.AppendText(text);
-			txtLog.AppendText(Environment.NewLine);
-			txtLog.SelectionStart = Int32.MaxValue;
-		}
-
-		void AppendNewLine() => txtLog.AppendText(Environment.NewLine);
 	}
+
+	void DisplayCompanies(string accessToken, string clusterUrl)
+	{
+		var processXmlService = new ProcessXmlService(clusterUrl);
+
+		var result = processXmlService.GetCompanies(accessToken);
+
+		var doc = XDocument.Parse(result);
+		var companies = doc.Root?.Elements("office").ToArray();
+
+		Log($"Access token : {accessToken}");
+		AppendNewLine();
+		Log($"Cluster url : {clusterUrl}");
+		AppendNewLine();
+		Log($"Found {companies?.Length} companies");
+		AppendNewLine();
+		Log($"Displaying first 10 companies");
+		AppendNewLine();
+
+		if (companies == null || !companies.Any()) 
+			return;
+
+		foreach (var company in companies.Take(10))
+			Log($"{company.Attribute("name")?.Value} [{company.Value}]");
+	}
+
+	void Log(string text)
+	{
+		txtLog.AppendText(text);
+		txtLog.AppendText(Environment.NewLine);
+		txtLog.SelectionStart = Int32.MaxValue;
+	}
+
+	void AppendNewLine() => txtLog.AppendText(Environment.NewLine);
 }
